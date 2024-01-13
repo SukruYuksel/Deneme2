4f9169cfc9f7eefaba7470d12288c46339329742
SukruYuksel <yksl3743dc@gmail.com>
2024-01-14 00:22:55 +0100

    Main deki hata giderildi

--- src/Reverse.java
+++ src/Reverse.java
@@ -3,10 +3,11 @@ public class Reverse {
 
         String str1="abc";
         String reverse="";
-        for (int i = str1.length()-1; i>=0 ; i--) 
-            reverse+=str1.charAt(i);
+        for (int i = str1.length()-1; i>=0 ; i--) {
+            reverse += str1.charAt(i);
+
         }
         System.out.println(reverse);
-  System.out.println("Reverse Codu Yazildi");
+        System.out.println("Cod basariyla eklendi");
     }
 }
